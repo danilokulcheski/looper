6f14f2da73703163348ac8cc5f9eb7368978feb9
danilokulcheski <105357863+danilokulcheski@users.noreply.github.com>
2023-10-24 07:51:31 -0300

    teclass

--- main.js
+++ main.js
@@ -9,13 +9,13 @@ document.querySelector('.tecla_pom').onclick = tocaSom;
 const listaDeTeclas = document.querySelectorAll('.tecla');
 
 //atribuindo o clique no botão "0" que é 1 item da lista de teclas.
-listaDeTeclas[0].onclick = tocaSomPom;
+listaDeTeclas[0].onclick = tocaSom;
 //criando referencia variável, atribuindo o valor inicial como "0"
 let contador = 0;
 //Criando o código de repetição WHILE
 //enquanto o contador for menos que o comprimento da lista, tocará o som.
 while(contador < listaDeTeclas.length) {
-    listaDeTeclas[contador].onclick = tocaSomPom;
+    listaDeTeclas[contador].onclick = tocaSom;
     contador = contador + 1;
     console.log(contador);
 }
