a6a72e489be826511af206bce17438ffabe033ec
AndersonCRF <105357863+danilokulcheski@users.noreply.github.com>
2023-11-21 07:39:51 -0300

    blz mané

--- main.js
+++ main.js
@@ -8,7 +8,7 @@ const listaDeTeclas = document.querySelectorAll('.tecla');
 
 //Criando o código de repetição WHILE e mudando para FOR
 //enquanto o contador for menos que o comprimento da lista, tocará o som.
-for(let contador = 0;contador < listaDeTeclas.length;contador = contador++); 
+for(let contador = 0;contador < listaDeTeclas.length;contador = contador++){
     const tecla = listaDeTeclas[contador];
     const efeito = tecla.classList[1]; //chama a classe do indice 1 do som
     const idAudio = `#som_${efeito}`; //junção com template string
@@ -16,8 +16,9 @@ for(let contador = 0;contador < listaDeTeclas.length;contador = contador++);
         tocaSom(idAudio);
     }
     tecla.onkeydown = function (evento) { //função para tecla pressionada
-       if (EventCounts.code != 'tab'){
+       if (evento.code === 'Space' || evento.code === 'Enter'){
          tecla.classList.add('ativa');
+       }
     }
     tecla.onkeyup = function(){ //função para tecla despressionada
         tecla.classList.remove('ativa');
